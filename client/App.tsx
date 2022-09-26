828eadfa36b77c5b634a0e2a458225bbb3213587
devallama <me@nicholasneale.com>
2022-09-26 05:46:13 -0700

    Add swrconfig

--- client/App.tsx
+++ client/App.tsx
@@ -1,14 +1,25 @@
 import React from 'react';
 import { RouterProvider } from 'react-router-dom';
+import { SWRConfig } from 'swr';
+import fetcher from './lib/fetcher';
 
 import { ThemeProvider } from 'styled-components';
 import theme from 'Theme/lightTheme';
 import router from './router';
 
+import { GlobalStyles } from 'Global/Global.styled';
+
 const App = () => (
-    <ThemeProvider theme={theme}>
-        <RouterProvider router={router} />
-    </ThemeProvider>
+    <SWRConfig
+        value={{
+            fetcher
+        }}
+    >
+        <ThemeProvider theme={theme}>
+            <GlobalStyles />
+            <RouterProvider router={router} />
+        </ThemeProvider>
+    </SWRConfig>
 );
 
 export default App;
